f282e709894c0d1d603935f1e6d07b15fe62cfe6
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-05-16 20:26:34 +0600

    display song lyric

--- script.js
+++ script.js
@@ -69,6 +69,18 @@ async function getMoreSongs(url) {
     showData(data);
   }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2><span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+}
+
 // Event listener
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -81,3 +93,15 @@ form.addEventListener('submit', e => {
         searchSongs(searchTerm);
     }
 })
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+  
+    if (clickedEl.tagName === 'BUTTON') {
+      const artist = clickedEl.getAttribute('data-artist');
+      const songTitle = clickedEl.getAttribute('data-songtitle');
+  
+      getLyrics(artist, songTitle);
+    }
+  });
